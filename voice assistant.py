2e280464a9aa742fb2a6f23f7bb3f85149a75cb7
Anirban-2003 <142056884+Anirban-2003@users.noreply.github.com>
2024-01-31 01:28:19 +0530

    Add files via upload

---
+++ voice assistant.py
@@ -0,0 +1,19 @@
+import pyttsx3
+import openai
+
+openai.api_key = "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"
+user_input=input()
+
+messages = [{"role": "system", "content": "chatbot"}]
+messages.append({"role": "user", "content": user_input})
+
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=messages
+)
+
+ChatGPT_reply = response["choices"][0]["message"]["content"]
+text_speech = pyttsx3.init()
+text_speech.say(ChatGPT_reply)
+print(ChatGPT_reply)
+text_speech.runAndWait()
