8fee55ad0444f2aeb2782b97f6027c1931bb1a8e
Anirban-2003 <142056884+Anirban-2003@users.noreply.github.com>
2024-01-31 02:12:34 +0530

    Update voice assistant.py

--- voice assistant.py
+++ voice assistant.py
@@ -1,19 +1,19 @@
-import pyttsx3
 import openai
+import pyttsx3
 
 openai.api_key = "###"
-user_input=input()
 
 messages = [{"role": "system", "content": "chatbot"}]
-messages.append({"role": "user", "content": user_input})
-
-response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=messages
-)
-
-ChatGPT_reply = response["choices"][0]["message"]["content"]
-text_speech = pyttsx3.init()
-text_speech.say(ChatGPT_reply)
-print(ChatGPT_reply)
-text_speech.runAndWait()
+for _ in range(5):
+    user_input=input()
+    messages.append({"role": "user", "content": user_input})
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages = messages
+    )
+    ChatGPT_reply = response["choices"][0]["message"]["content"]
+    messages.append({"role": "assistant", "content": ChatGPT_reply})
+    text_speech = pyttsx3.init()
+    text_speech.say(ChatGPT_reply)
+    print(ChatGPT_reply)
+    text_speech.runAndWait()
